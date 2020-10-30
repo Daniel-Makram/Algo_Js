202cd1a282801b03c046bb56baed17b21e4c0c77
Daniel-Makram <47518290+Daniel-Makram@users.noreply.github.com>
2020-10-30 16:04:59 +0100

    Add files via upload

---
+++ Exo_5-2.js
@@ -0,0 +1,24 @@
+// EXO 5.2-------------------------------------------------------------------------------------------------------------------------------
+
+// NameOfCast = ["Fry", "Layla", "Scruffy", "Dr. Zoickberg"];
+// NewCast = [];
+
+// // !!!!!TODO!!!!! Need to fix problem when number of cast=0 and when i push a cast member who's number has already been spliced
+
+// let randomizeCast = (tvSerie) => {
+
+//     RndNbrCast = Math.floor((Math.random()) * (NameOfCast.length));
+//     console.log("Number of Cast Members " + RndNbrCast);
+
+//     for (i = 1; i <= RndNbrCast; i++) {
+//         console.log("made it to the loop");
+
+//         RndDude = Math.floor((Math.random()) * (RndNbrCast));
+//         console.log(RndDude);
+
+//         NewCast.push(NameOfCast[RndDude]);
+//         NameOfCast.splice(RndDude, 1);
+//     }
+//     console.log(NewCast);
+// }
+
